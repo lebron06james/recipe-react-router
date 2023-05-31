b1bc42c558b96d8ab2feb5ac561210f754170dd4
Borge <borge@milkyway.com>
2023-05-31 16:00:47 +0800

    lakers in 12

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,4 @@
-export const waait = () => new Promise(res => setTimeout(res, Math.random() * 2000))
+export const waait = () => new Promise(res => setTimeout(res, Math.random() * 800))
 
 
 // colors
@@ -28,7 +28,56 @@ export const createBudget = ({
     JSON.stringify([...existingBudgets, newItem]))
 }
 
+// create expense
+export const createExpense = ({
+  name, amount, budgetId
+}) => {
+  const newItem = {
+    id: crypto.randomUUID(),
+    name: name,
+    createdAt: Date.now(),
+    amount: +amount,
+    budgetId: budgetId
+  }
+  const existingExpenses = fetchData("expenses") ?? [];
+  return localStorage.setItem("expenses",
+    JSON.stringify([...existingExpenses, newItem]))
+}
+
 // delete item
 export const deleteItem = ({ key }) => {
   return localStorage.removeItem(key)
+}
+
+// total spent by budget
+export const calculateSpentByBudget = (budgetId) => {
+  const expenses = fetchData("expenses") ?? [];
+  const budgetSpent = expenses.reduce((acc, expense) => {
+    // check if expense.id === budgetId I passed in
+    if (expense.budgetId !== budgetId) return acc
+
+    // add the current amount to my total
+    return acc += expense.amount
+  }, 0)
+  return budgetSpent;
+}
+
+
+// FORMATTING
+export const formatDateToLocaleString = (epoch) => new Date(epoch).toLocaleDateString();
+
+// Formating percentages
+export const formatPercentage = (amt) => {
+  return amt.toLocaleString(undefined, {
+    style: "percent",
+    minimumFractionDigits: 0,
+  })
+}
+
+// Format currency
+export const formatCurrency = (amt) => {
+  return amt.toLocaleString(undefined, {
+    style: "currency",
+    currency: "USD"
+  })
 }
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -7,15 +7,19 @@ import { toast } from "react-toastify";
 // components
 import Intro from "../components/Intro";
 import AddBudgetForm from "../components/AddBudgetForm";
+import AddExpenseForm from "../components/AddExpenseForm";
+import BudgetItem from "../components/BudgetItem";
+import Table from "../components/Table";
 
 //  helper functions
-import { createBudget, fetchData, waait } from "../helpers"
+import { createBudget, createExpense, fetchData, waait } from "../helpers"
 
 // loader
 export function dashboardLoader() {
   const userName = fetchData("userName");
   const budgets = fetchData("budgets");
-  return { userName, budgets }
+  const expenses = fetchData("expenses");
+  return { userName, budgets, expenses }
 }
 
 // action
@@ -46,10 +50,23 @@ export async function dashboardAction({ request }) {
       throw new Error("There was a problem creating your budget.")
     }
   }
+
+  if (_action === "createExpense") {
+    try {
+      createExpense({
+        name: values.newExpense,
+        amount: values.newExpenseAmount,
+        budgetId: values.newExpenseBudget
+      })
+      return toast.success(`Expense ${values.newExpense} created!`)
+    } catch (e) {
+      throw new Error("There was a problem creating your expense.")
+    }
+  }
 }
 
 const Dashboard = () => {
-  const { userName, budgets } = useLoaderData()
+  const { userName, budgets, expenses } = useLoaderData()
 
   return (
     <>
@@ -57,12 +74,40 @@ const Dashboard = () => {
         <div className="dashboard">
           <h1>Welcome back, <span className="accent">{userName}</span></h1>
           <div className="grid-sm">
-            {/* {budgets ? () : ()} */}
-            <div className="grid-lg">
-              <div className="flex-lg">
-                <AddBudgetForm />
-              </div>
-            </div>
+            {
+              budgets && budgets.length > 0
+                ? (
+                  <div className="grid-lg">
+                    <div className="flex-lg">
+                      <AddBudgetForm />
+                      <AddExpenseForm budgets={budgets} />
+                    </div>
+                    <h2>Existing Budgets</h2>
+                    <div className="budgets">
+                      {
+                        budgets.map((budget) => (
+                          <BudgetItem key={budget.id} budget={budget} />
+                        ))
+                      }
+                    </div>
+                    {
+                      expenses && expenses.length > 0 && (
+                        <div className="grid-md">
+                          <h2>Recent Expenses</h2>
+                          <Table expenses={expenses.sort((a, b) => b.createdAt - a.createdAt)} />
+                        </div>
+                      )
+                    }
+                  </div>
+                )
+                : (
+                  <div className="grid-sm">
+                    <p>Personal budgeting is the secret to financial freedom.</p>
+                    <p>Create a budget to get started!</p>
+                    <AddBudgetForm />
+                  </div>
+                )
+            }
           </div>
         </div>
       ) : <Intro />}
